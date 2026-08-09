d8d6b60315f78bfc3feb5b06466fcfb3e6de5f67
Uday Tyagi <146240633+20-uday-06@users.noreply.github.com>
2026-08-09 16:55:12 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -0,0 +1,35 @@
+class TimeMap {
+public:
+    unordered_map<string, vector<pair<int, string>>> mp; // {key , {time , value}}
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({timestamp , value});
+    }
+    
+    string get(string key, int timestamp) {
+        string ans = "";
+        if(mp.find(key) == mp.end()) return "";
+
+        auto& arr = mp[key];
+        int left = 0;
+        int right = arr.size()-1;
+
+        while(left <= right){
+            int mid = left + (right-left)/2;
+
+            // if(arr[mid].first == timestamp) return arr[mid].second;
+
+            if(arr[mid].first <= timestamp){
+                ans = arr[mid].second;
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
+            }
+        }
+        return ans;
+    }
+};
