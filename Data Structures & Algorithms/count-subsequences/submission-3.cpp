b925eb0bc8a367889f049ed1cc9cc25857bba463
Uday Tyagi <146240633+20-uday-06@users.noreply.github.com>
2026-08-19 01:26:19 +0530

    Add: count-subsequences - submission-3

---
+++ Data Structures & Algorithms/count-subsequences/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+   public:
+    int solve(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
+        if (j == t.size()) return 1;
+        if (i == s.size()) return 0;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (s[i] == t[j]) {
+            int take = solve(i + 1, j + 1, s, t, dp);
+            int notTake = solve(i + 1, j, s, t, dp);
+
+            return dp[i][j] = take + notTake;
+        }
+        return dp[i][j] = solve(i + 1, j, s, t, dp);
+    }
+
+    int numDistinct(string s, string t) {
+        vector<vector<int>> dp(s.size(), vector<int>(t.size(), -1));
+
+        return solve(0, 0, s, t, dp);
+    }
+};
